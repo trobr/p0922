8397aa410d7e4fd6fd0f2c6a90864095793d0dad
trobr <wyf5249@126.com>
2025-09-28 23:32:06 +0800

    add test

--- test.py
+++ test.py
@@ -3,7 +3,8 @@ import hydra
 import pytorch_lightning as pl
 
 
-@hydra.main(config_path="./confs", config_name="gala", version_base="1.1")
+# @hydra.main(config_path="./confs", config_name="gala", version_base="1.1")
+@hydra.main(config_path="./confs", config_name="peoplesnapshot", version_base="1.1")
 def main(opt):
     pl.seed_everything(0)
 
